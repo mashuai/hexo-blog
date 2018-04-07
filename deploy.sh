54a9ba88140f9e6dd7e0d7dfa877ec4743b0fbd6
mashuai <mashuai@adesk.com>
2018-04-07 20:59:04 +0800

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-
 git add .
 git commit -m 'auto commit'
 git push origin master
-hexo g && hexo d
+
+echo "generate to html"
+
+hexo g
+
+echo "deploy to coding"
+cp _config.coding.yml _config.yml
+hexo d
+
+echo "deploy to github"
+cp _config.github.yml _config.yml
+hexo d
